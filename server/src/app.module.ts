8a78d71039950d5ae74cff7a1d228eb0d5094ade
Michael Gift <simiyugift@outlook.com>
2026-04-09 22:48:09 +0300

    chore(app-module) - Refactor app module for readability.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dbConfig from './config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const config = {
+  isGlobal: true,
+  load: [
+    dbConfig
+  ],
+  envFilePath: '.env',
+}
+
+const dbConfigFactory = {
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => {
+    console.log('🔌 Attempting to connect to PostgreSQL...');
+    return configService.get('db')!;
+  },
+  inject: [ConfigService],
+}
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(config),
+    TypeOrmModule.forRootAsync(dbConfigFactory)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
