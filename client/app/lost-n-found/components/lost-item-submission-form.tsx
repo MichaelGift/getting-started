402872338d220225338b7e0ca83fab188afbded4
Asia Osman <asiaosman06@gmail.com>
2026-04-20 22:42:21 +0300

    feat(Components) - Enhance lost item submission form with improved styling and structure

--- client/app/lost-n-found/components/lost-item-submission-form.tsx
+++ client/app/lost-n-found/components/lost-item-submission-form.tsx
@@ -25,33 +25,56 @@ export default function LostItemSubmissionForm() {
 
 
     return (
-        <form onSubmit={submitForm}>
+      <div className="w-full max-w-xs bg-black">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 "
+          onSubmit={submitForm}
+        >
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Name
+            </label>
             <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder={"Name of the item"}
+              className="shadow apperance-none border rounded w-full py-2 px-3"
+              // value={name}
+              // onChange={(e) => setName(e.target.value)}
+              placeholder={"Name of the item"}
+              required
+              type={"text"}
+            />
+          </div>
 
-                required
-                type={"text"}
-            />
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Color
+            </label>
             <input
-                value={color}
-                onChange={(e) => setColor(e.target.value)}
-                placeholder={"color of the item"}
-                required
-                type={"text"}
+              className="shadow apperance-none border rounded w-full py-2 px-3"
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+              placeholder={"color of the item"}
+              required
+              type={"text"}
             />
-            <input
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder={"description of the item"}
-                required
-                type={"text"}
-            />
-            <button type="submit">
-                Submit
-            </button>
+          </div>
 
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Description
+            </label>
+            <input
+              className="shadow apperance-none border rounded w-full py-2 px-3"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder={"description of the item"}
+              required
+              type={"text"}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded" type="submit">Submit</button>
+          </div>
         </form>
-    )
-}
+      </div>
+    );
+} 
