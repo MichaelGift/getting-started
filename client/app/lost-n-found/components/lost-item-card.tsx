16c1c721c45d8d95aa60fb26535c32e2e30b9cad
MichaelGift <simiyugift@outlook.com>
2026-04-20 23:33:22 +0300

    fix(UX) - Remove excess item info

--- client/app/lost-n-found/components/lost-item-card.tsx
+++ client/app/lost-n-found/components/lost-item-card.tsx
@@ -2,16 +2,20 @@
 
 import {deleteItem} from "@/app/lost-n-found/action";
 import {Item} from "@/app/data/lost-items";
+import {useRouter} from "next/navigation";
 
 export default function LostItemCard({ data }: { data: Item }) {
-    return <>
-        <i>{data.id}</i>
+    const router = useRouter()
+    return <div className="shadow-sm">
+        {/*<i>{data.id}</i>*/}
         <h1>{data.name}</h1>
         <p>{data.description}</p>
-        <i>{data.createdAt}</i>
+        <p>{data.color}</p>
+        {/*<i>{data.createdAt}</i>*/}
         <hr/>
         <button onClick={() => deleteItem(data.id)}>
             Delete
         </button>
-    </>
+        <button onClick={() => router.push(`lost-n-found/${data.id}`) }>Edit</button>
+    </div>
 }
